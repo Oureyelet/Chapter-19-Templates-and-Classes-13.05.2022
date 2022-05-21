bfe8b14167b1e83dc2e3df442021bbcb5e58dee3
Mateusz <oureyelet@gmail.com>
2022-05-21 09:45:31 +0100

    -

--- 19.6 — Partial template specialization for pointers (vsCode)/main.cpp
+++ 19.6 — Partial template specialization for pointers (vsCode)/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <cstring>
 
+// Our Storage class for non-pointers
 template<typename T>
 class Storage
 {
@@ -16,12 +18,36 @@ public:
     {
     }
 
-    void print()
+    void print() const
     {
         std::cout << m_value << '\n';
     }
 };
 
+template<typename T>
+class Storage<T*> // this is a partial-specialization of Storage that works with pointer types
+{
+private:
+    T* m_value{};
+
+public:
+    Storage(T* value)// for pointer type T
+        : m_value{ new T{ *value } }// this copies a single value, not an array
+    {
+    }
+
+    ~Storage()
+    {
+        delete m_value;// so we use scalar delete here, not array delete
+    }
+
+    void print() const
+    {
+        std::cout << *m_value << '\n';
+    }
+};
+
+// Full specialization of constructor for type char*
 template<>
 Storage<char*>::Storage(char* value)
 {
@@ -40,20 +66,61 @@ Storage<char*>::Storage(char* value)
         m_value[count] = value[count];   
 }
 
+// Full specialization of destructor for type char*
 template<>
 Storage<char*>::~Storage()
 {
     delete[] m_value; 
 }
 
+// Full specialization of print function for type char*
+// Without this, printing a Storage<char*> would call Storage<T*>::print(), which only prints the first char
+template<>
+void Storage<char*>::print() const
+{
+    std::cout << m_value << '\n';
+}
+
+
+
 int main()
 {
     std::cout << "Hello World my name is Sophie\n";
 
-    /*
-    While that worked great for Storage<char*>, what about other pointer types (such as int*)? 
-    It’s fairly easy to see that if T is any pointer type, then we run i...
-    */
+    int a{ 5 };
+
+    int* b{ &a };
+
+    std::cout << "'a' value: " << a << '\n';
+    std::cout << "'a' addres: " << &a << '\n';
+    std::cout << "'b' addres: " << b << '\n';
+    std::cout << "'b' value: " << *b << '\n';
+    std::cout << "'b pointer' addres: " << &b << '\n';
+
+    // Declare a non-pointer Storage to show it works
+    Storage<int> myint{ 5 };
+    myint.print();
+
+    // Declare a pointer Storage to show it works
+    int x{ 77 };
+    Storage<int*> myintptr{ &x };
+
+    // Let's show that myintptr is separate from x.
+	// If we change x, myintptr should not change
+    x = 122;
+    myintptr.print();
+
+    // Dynamically allocate a temporary string
+    char* name{ new char[40]{ "Alex" } };
+
+    // Store the name
+    Storage<char*> myName{ name };
+
+    // Delete the temporary string
+    delete[] name;
+
+    // Print out our name to prove we made a copy
+    myName.print();
 
     return 0;
 }
